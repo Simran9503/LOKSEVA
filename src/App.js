c813b8f8a94884c943416f6269fd1723e3b97a0a
Simran Pachauri <simranapchauri8@gmail.com>
2023-06-24 13:38:04 +0530

    this is it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="App"  >
+<Box sx={{
+          backgroundImage: `url('/public/images/image.jpg')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+}}>
+      <Router>
+        <Navbar />
+      </Router>
+      </Box>
     </div>
+
+
+   
   );
 }
 
